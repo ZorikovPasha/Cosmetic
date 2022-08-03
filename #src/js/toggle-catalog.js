5d34f750d0e1f27da4fbb1bb01fd91a7d08fbe73
ZorikovPasha <zorikov.pasha@mail.ru>
2022-08-03 22:51:30 +0300

    made some edits

--- #src/js/toggle-catalog.js
+++ #src/js/toggle-catalog.js
@@ -1,22 +1,16 @@
-$('.aside__link').on('click', function() {
-  $('.catalog').addClass('shown')
-})
-
-$('.aside__link').on('click', function() {
-  $('.catalog').addClass('opened')
-
-  setTimeout( function() {
-    $('.catalog__body').addClass('opened')
-  }, 200)
-  $('body').addClass('lock')
-})
-
-$('.catalog__close').on('click', function() {
-  $('.catalog__body').removeClass('opened')
-
-  setTimeout( function() {
-    $('.catalog').removeClass('opened')
-  }, 200)
-
-  $('body').removeClass('lock')
+$(function() {
+  $('.aside__link').on('click', () => {
+    $('.catalog').addClass('opened')
+  
+    setTimeout(() => $('.catalog__body').addClass('opened'), 200)
+    $('body').addClass('lock')
+  })
+  
+  $('.catalog__close, .catalog__list-item').on('click', () => {
+    $('.catalog__body').removeClass('opened')
+  
+    setTimeout(() => $('.catalog').removeClass('opened'), 200)
+  
+    $('body').removeClass('lock')
+  })
 })
